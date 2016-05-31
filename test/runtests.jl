b0b1f7a2ff68db8deadc2d5f65db60c0b84a8c38
Gio Cielo <gccielo@gmail.com>
2016-05-31 01:27:51 -0700

    Add simple serialization test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,28 @@
 using Avro
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+type User
+  name::ASCIIString
+  age::UInt32
+end
+
+user = User("hello", 22)
+expected_bytes = [
+ 0x05
+ 0x00
+ 0x00
+ 0x00
+ 0x00
+ 0x00
+ 0x00
+ 0x00
+ 0x68
+ 0x65
+ 0x6c
+ 0x6c
+ 0x6f
+ 0x2c
+]
+stream = IOBuffer(length(expected_bytes))
+Avro.serialize(stream, user)
+@test expected_bytes == stream.data
