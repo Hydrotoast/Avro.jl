713afa171c59c5f5aa2492889adbe0b07a958277
Gio Cielo <gccielo@gmail.com>
2017-02-05 10:28:08 -0800

    Test default writers for primitive types

--- test/io.jl
+++ test/io.jl
@@ -6,40 +6,40 @@ using Base.Test
 
 const BOOLEAN_EXAMPLES = 
     [
-        (true, [0x01]),
-        (false, [0x00]),
+        (true, Schemas.BOOLEAN, [0x01]),
+        (false, Schemas.BOOLEAN, [0x00]),
     ]
 
 const INT_EXAMPLES = 
     [
-        (Int32(0), [0x00]),
-        (Int32(-1), [0x01]),
-        (Int32(1), [0x02]),
-        (Int32(-2), [0x03]),
-        (Int32(2), [0x04]),
-        (Int32(-64), [0x7f]),
-        (Int32(64), [0x80, 0x01]),
-        (Int32(8192), [0x80, 0x80, 0x01]),
-        (Int32(-8193), [0x81, 0x80, 0x01]),
+        (Int32(0), Schemas.INT, [0x00]),
+        (Int32(-1), Schemas.INT, [0x01]),
+        (Int32(1), Schemas.INT, [0x02]),
+        (Int32(-2), Schemas.INT, [0x03]),
+        (Int32(2), Schemas.INT, [0x04]),
+        (Int32(-64), Schemas.INT, [0x7f]),
+        (Int32(64), Schemas.INT, [0x80, 0x01]),
+        (Int32(8192), Schemas.INT, [0x80, 0x80, 0x01]),
+        (Int32(-8193), Schemas.INT, [0x81, 0x80, 0x01]),
     ]
 
 const LONG_EXAMPLES = 
     [
-        (Int64(0), [0x00]),
-        (Int64(-1), [0x01]),
-        (Int64(1), [0x02]),
-        (Int64(-2), [0x03]),
-        (Int64(2), [0x04]),
-        (Int64(-64), [0x7f]),
-        (Int64(64), [0x80, 0x01]),
-        (Int64(8192), [0x80, 0x80, 0x01]),
-        (Int64(-8193), [0x81, 0x80, 0x01]),
+        (Int64(0), Schemas.LONG, [0x00]),
+        (Int64(-1), Schemas.LONG, [0x01]),
+        (Int64(1), Schemas.LONG, [0x02]),
+        (Int64(-2), Schemas.LONG, [0x03]),
+        (Int64(2), Schemas.LONG, [0x04]),
+        (Int64(-64), Schemas.LONG, [0x7f]),
+        (Int64(64), Schemas.LONG, [0x80, 0x01]),
+        (Int64(8192), Schemas.LONG, [0x80, 0x80, 0x01]),
+        (Int64(-8193), Schemas.LONG, [0x81, 0x80, 0x01]),
     ]
 
 const STRING_EXAMPLES = 
     [
-        ("foo", [0x06, 0x66, 0x6f, 0x6f])
-        ("", [0x00])
+        ("foo", Schemas.STRING, [0x06, 0x66, 0x6f, 0x6f])
+        ("", Schemas.STRING, [0x00])
     ]
 
 const ARRAY_EXAMPLES = 
@@ -64,32 +64,56 @@ buffer = IOBuffer()
 encoder = BinaryEncoder(buffer)
 
 @testset "Encoding" begin
-    @testset "Integer" for (input, expected) in INT_EXAMPLES
+    @testset "Integer" for (input, schema, expected) in INT_EXAMPLES
         bytes_written = encode(encoder, input)
+        contents = takebuf_array(buffer)
+
+        @test expected == contents
+        @test length(expected) == bytes_written
+
+        bytes_written = write(encoder, schema, input)
         contents = takebuf_array(buffer)
 
         @test expected == contents
         @test length(expected) == bytes_written
     end
 
-    @testset "Long" for (input, expected) in LONG_EXAMPLES
+    @testset "Long" for (input, schema, expected) in LONG_EXAMPLES
         bytes_written = encode(encoder, input)
+        contents = takebuf_array(buffer)
+
+        @test expected == contents
+        @test length(expected) == bytes_written
+
+        bytes_written = write(encoder, schema, input)
         contents = takebuf_array(buffer)
 
         @test expected == contents
         @test length(expected) == bytes_written
     end
 
-    @testset "Boolean" for (input, expected) in BOOLEAN_EXAMPLES
+    @testset "Boolean" for (input, schema, expected) in BOOLEAN_EXAMPLES
         bytes_written = encode(encoder, input)
+        contents = takebuf_array(buffer)
+
+        @test expected == contents
+        @test length(expected) == bytes_written
+
+        bytes_written = write(encoder, schema, input)
         contents = takebuf_array(buffer)
 
         @test expected == contents
         @test length(expected) == bytes_written
     end
 
-    @testset "String" for (input, expected) in STRING_EXAMPLES
+    @testset "String" for (input, schema, expected) in STRING_EXAMPLES
         bytes_written = encode(encoder, input)
+        contents = takebuf_array(buffer)
+
+        @test expected == contents
+        @test length(expected) == bytes_written
+
+        bytes_written = write(encoder, schema, input)
         contents = takebuf_array(buffer)
 
         @test expected == contents
